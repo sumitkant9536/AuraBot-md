7d45676f2eefc3bce3dd1ec6ad949a397a5c4ec2
RFIunknown <97925894+RFIunknown@users.noreply.github.com>
2022-05-21 23:42:10 +0800

    Update AuraBot-md

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-import express from 'express'
-import { createServer } from 'http'
-import path from 'path'
-import { Socket } from 'socket.io'
-import { toBuffer } from 'qrcode'
-import fetch from 'node-fetch'
+let express = require('express')
+let path = require('path')
+let SocketIO = require('socket.io')
+let qrcode = require('qrcode')
+let fetch = require('node-fetch')
 
 function connect(conn, PORT) {
     let app = global.app = express()
-    console.log(app)
-    let server = global.server = createServer(app)
-    // app.use(express.static(path.join(__dirname, 'views')))
+
+    //app.use(express.static(path.join(__dirname, 'views')))
     let _qr = 'invalid'
 
     conn.ev.on('connection.update', function appQR({ qr }) {
@@ -18,18 +16,18 @@ function connect(conn, PORT) {
 
     app.use(async (req, res) => {
         res.setHeader('content-type', 'image/png')
-        res.end(await toBuffer(_qr))
+        res.end(await qrcode.toBuffer(_qr))
     })
 
-    // let io = new Socket(server)
-    // io.on('connection', socket => {
-    //     let { unpipeEmit } = pipeEmit(conn, socket, 'conn-')
-    //     socket.on('disconnect', unpipeEmit)
-    // })
+    let server = app.listen(PORT, () => {
+      console.log('Aplikasi berjalan di port', PORT)
+      if (opts['keepalive']) keepAlive()
+    })
 
-    server.listen(PORT, () => {
-        console.log('App listened on port', PORT)
-        if (opts['keepalive']) keepAlive()
+    let io = SocketIO(server)
+    io.on('connection', socket => {
+        let { unpipeEmit } = pipeEmit(conn, socket, 'conn-')
+        socket.on('disconnect', unpipeEmit)
     })
 }
 
@@ -47,12 +45,11 @@ function pipeEmit(event, event2, prefix = '') {
 }
 
 function keepAlive() {
-    const url = `https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`
-    if (/(\/\/|\.)undefined\./.test(url)) return
-    setInterval(() => {
-        fetch(url).catch(console.error)
-    }, 5 * 1000 * 60)
+  const url = `https://${process.env.REPL_SLUG}.${process.env.REPL_OWNER}.repl.co`
+  if (/(\/\/|\.)undefined\./.test(url)) return
+  setInterval(()=> {
+    fetch(url).catch(console.error)
+  }, 5 * 1000 * 60)
 }
 
-
-export default connect
+module.exports = connect
