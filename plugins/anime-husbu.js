687772079321f2746ee2e85db33a57562d45e009
RFIunknown <97925894+RFIunknown@users.noreply.github.com>
2022-05-25 19:07:34 +0800

    Update anime-husbu.js

--- plugins/anime-husbu.js
+++ plugins/anime-husbu.js
@@ -1,15 +1,8 @@
-let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
-  let res = await fetch('https://rest-beni.herokuapp.com/api/randomimage/husbu')
-  if (!res.ok) throw 'Error Website sedang down'
-  let json = await res.json()
-  if (!json.url) throw 'Error!'
-  conn.sendFile(m.chat, json.url, '', 'istri gweh', m, 0, { thumbnail: Buffer.alloc(0) })
+    conn.sendButtonImg(m.chat, global.API('https://rest-beni.herokuapp.com/api/randomimage/husbu'), 'Nih', wm, 'NEXT', '.husbu', m)
 }
 handler.help = ['husbu']
 handler.tags = ['anime']
 handler.command = /^(husbu)$/i
-
-handler.limit = true
 
 module.exports = handler
