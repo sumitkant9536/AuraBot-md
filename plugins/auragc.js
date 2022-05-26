fc11ae3ce46e8422539e5164d86ed99efd5e1299
RFIunknown <97925894+RFIunknown@users.noreply.github.com>
2022-05-26 14:22:28 +0800

    Update auragc.js

--- plugins/auragc.js
+++ plugins/auragc.js
@@ -1,15 +1,14 @@
-let fetch = require('node-fetch')
+let fetch from 'node-fetch'
 let handler = async(m, { conn }) => {
     try {
-        let pp = await(await fetch(image)).buffer
-        let gc1 = '6281393227036-120363037529652885@g.us'
-        let gc2 = '6281393227036-120363041039565084@g.us'
+        let gc1 = '628112958665-1625393837@g.us'
+        let gc2 = '628112958665-1628163967@g.us'
         let gc3 = '628112958665-1571053173@g.us'
         let _gc1 = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(gc1)
         let _gc2 = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(gc2)
         let _gc3 = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(gc3)
         let caption = `
-*Join juga group official AuraBot!*
+*Join juga group official Haruno Bot!*
 Sekedar meramaikan, atau juga jika ada informasi mengenai bot.
 *Group 1*
 ${_gc1}
@@ -19,19 +18,12 @@ ${_gc2}
 ${_gc3}
 Harap patuhi rules dan peraturan pada masing masing group ya! Terimakasih.
 `.trim()
-        await conn.reply(m.chat, caption, m, { contextInfo: {
-            externalAdReply: {
-              sourceUrl: 'https://rfiunknown.github.io/dist/',
-              title: 'Official group',
-              body: 'AuraBot',
-              thumbnail: pp
-            }
-        }})
+        await conn.reply(m.chat, caption, m)
     } catch (e) {
-        let res = await fetch('https://raw.githubusercontent.com/RFIunknown/AuraBot-server/main/files/src.json')
+        let res = await fetch('https://raw.githubusercontent.com/FadliDarmawan/haruno-server/main/files/src.json')
         let json = await res.json()
         let tulisan = `
-*Join juga group official AuraBot!*
+*Join juga group official Haruno Bot!*
 Sekedar meramaikan, atau juga jika ada informasi mengenai bot.
 *Group 1*
 ${json.links[0]}
@@ -41,17 +33,10 @@ ${json.links[1]}
 ${json.links[2]}
 Harap patuhi rules dan peraturan pada masing masing group ya! Terimakasih.
 `.trim()
-    await conn.reply(m.chat, tulisan, m, { contextInfo: {
-        externalAdReply: {
-            sourceUrl: 'https://rfiunknown.github.io/dist/',
-            title: 'Official group',
-            body: 'AuraBot',
-            thumbnail: pp
-        }
-    }})
+    await conn.reply(m.chat, tulisan, m)
     }
 }
 handler.help = ['groupofficial']
 handler.tags = ['main']
-handler.command = /^auragc|gr(u|ou)(p|b)official|gcofficial$/i
-module.exports = handler
+handler.command = /^harunoff|gr(u|ou)(p|b)official|gcofficial$/i
+export default handler
