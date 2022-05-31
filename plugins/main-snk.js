90a3c1c158d4ffc064ec30f6d18cb163ccbb9d4f
RFIunknown <97925894+RFIunknown@users.noreply.github.com>
2022-05-31 14:09:45 +0800

    update ubah dikit

--- plugins/main-snk.js
+++ plugins/main-snk.js
@@ -1,7 +1,9 @@
-let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
-*Kebijakan Privasi, Syarat Ketentuan dan Peraturan AuraBot*
+let fetch = require('node-fetch')
+let handler = async(m, { conn }) => {
+    let teks = 
+❖ *Kebijakan Privasi, Syarat Ketentuan dan Peraturan AuraBot* ❖
 
-◉ [*Kebijakan Privasi*]
+◉ [ *Kebijakan Privasi* ]
 1. AuraBot tidak akan merekam data riwayat chat user.
 2. AuraBot tidak akan menyebarkan nomor users.
 3. AuraBot tidak akan menyimpan media yang dikirimkan oleh users.
@@ -10,14 +12,14 @@ let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 6. Owner AuraBot berhak melihat status users.
 7. Owner AuraBot dapat melihat riwayat chat, dan media yang dikirimkan users.
 
-◉ [*Peraturan AuraBot*]
+◉ [ *Peraturan AuraBot* ]
 1. Users dilarang menelpon maupun memvideo call nomor bot.
 2. Users dilarang mengirimkan berbagai bug, virtex, dll ke nomor bot.
 3. Users diharap tidak melakukan spam dalam penggunaan bot.
 4. Users dilarang menambahkan nomor bot secara illegal, untuk menambahkan silahkan hubungi owner.
 5. Users diharap untuk tidak menyalah gunakan fitur fitur bot.
 
-◉ [*Syarat Ketentuan AuraBot*]
+◉ [ *Syarat Ketentuan AuraBot* ]
 1. Bot akan keluar dari group apabila sudah waktunya keluar.
 2. AuraBot dapan mem-ban users secara sepihak terlepas dari users salah atau tidak.
 3. AuraBot *tidak akan bertanggungjawab atas apapun yang users lakukan terhadap fitur bot.*
@@ -28,9 +30,17 @@ let handler = async (m, { conn, usedPrefix }) => conn.sendButton(m.chat, `
 -Muhammad Rifai
 Peraturan: 10 Januari 2022
 
-`.trim(), wm, 'Menu', usedPrefix + 'menu', m) // Tambah sendiri kalo mau
+`.trim()
+    conn.reply(m.chat, teks, m, { contextInfo: {
+        externalAdReply: {
+          sourceUrl: 'https://rfiunknown.github.io/dist/',
+          title: 'Rules',
+          body: 'AuraBot',
+          thumbnail: pp
+        }
+    }})
+}
 handler.help = ['peraturan']
-handler.tags = ['main']
 handler.command = /^(snk|syarat|peraturan|rules)$/i
-
+handler.tags = ['main']
 module.exports = handler
