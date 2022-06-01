71bfc877c54bff81965495c3c9ae0f891e9b5b9e
RFIunknown <97925894+RFIunknown@users.noreply.github.com>
2022-06-01 21:49:50 +0800

    fix

--- plugins/_antibadword.js
+++ plugins/_antibadword.js
@@ -12,7 +12,7 @@ handler.before = function (m, { isOwner, isBotAdmin }) {
     if (!chat.badword && !chat.isBanned && isBadword) {
         user.warning += 1
         this.send2Button(m.chat, `*Badword terdeteksi!*
-Warning: ${user.warning} / 5
+Warning: ${user.warning} / 10
 Jika warning mencapai 5 kamu akan dibanned
 
 ketik *#on antibadword* untuk menyalakan antibadword
