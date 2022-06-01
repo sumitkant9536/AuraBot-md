ac11034a9fbdd0607e010ec1ffaa4ac58745125b
RFIunknown <97925894+RFIunknown@users.noreply.github.com>
2022-06-01 22:12:10 +0800

    fix

--- plugins/game-tictactoe.js
+++ plugins/game-tictactoe.js
@@ -37,12 +37,12 @@ ${arr.slice(6).join('')}
 Menunggu @${room.game.currentTurn.split('@')[0]}
 Ketik *nyerah* untuk nyerah
 `.trim()
-            if (room.x !== room.o) await conn.sendButton(room.x, str, watermark, 'Nyerah', 'nyerah', m, {
+            if (room.x !== room.o) await conn.sendButton(room.x, str, wm, 'Nyerah', '.nyerah', m, {
                 contextInfo: {
                     mentionedJid: conn.parseMention(str)
                 }
             })
-            await conn.sendButton(room.o, str, watermark, 'Nyerah', 'nyerah', m, {
+            await conn.sendButton(room.o, str, wm, 'Nyerah', '.nyerah', m, {
                 contextInfo: {
                     mentionedJid: conn.parseMention(str)
                 }
