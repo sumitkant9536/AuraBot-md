b7a9bb20777b51e84f0da6357803516bbfed855f
RFIunknown <97925894+RFIunknown@users.noreply.github.com>
2022-06-03 15:08:11 +0800

    fix

--- plugins/game-tebakkata_ans.js
+++ plugins/game-tebakkata_ans.js
@@ -10,7 +10,7 @@ module.exports = {
             let json = JSON.parse(JSON.stringify(this.tebakkata[id][1]))
             // m.reply(JSON.stringify(json, null, '\t'))
             if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
-                global.DATABASE._data.users[m.sender].exp += this.tebakkata[id][2]
+                db.data.users[m.sender].exp[m.sender].exp += this.tebakkata[id][2]
                 m.reply(`*Benar!*\n+${this.tebakkata[id][2]} XP`)
                 clearTimeout(this.tebakkata[id][3])
                 delete this.tebakkata[id]
