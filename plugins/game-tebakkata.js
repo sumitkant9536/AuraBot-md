7b847c2729627faae7a83d9d18773086f7c7497d
RFIunknown <97925894+RFIunknown@users.noreply.github.com>
2022-06-03 15:04:23 +0800

    fix

--- plugins/game-tebakkata.js
+++ plugins/game-tebakkata.js
@@ -1,6 +1,8 @@
 let fetch = require('node-fetch')
+
 let timeout = 120000
 let poin = 500
+let src
 let handler = async (m, { conn, usedPrefix }) => {
     conn.tebakkata = conn.tebakkata ? conn.tebakkata : {}
     let id = m.chat
@@ -8,21 +10,20 @@ let handler = async (m, { conn, usedPrefix }) => {
         conn.reply(m.chat, 'Masih ada soal belum terjawab di chat ini', conn.tebakkata[id][0])
         throw false
     }
-    let res = await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/tebakkata.json')
-    if (!res.ok) throw await `${res.status} ${res.statusText}`
-    let data = await res.json()
-    let json = data[Math.floor(Math.random() * data.length)]
+    if (!src) src = await (await fetch(global.API('https://raw.githubusercontent.com', '/BochilTeam/database/master/games/tebakkata.json'))).json()
+    let json = src[Math.floor(Math.random() * src.length)]
+    if (!json) throw json
     let caption = `
 ${json.soal}
 Timeout *${(timeout / 1000).toFixed(2)} detik*
 Ketik ${usedPrefix}teka untuk bantuan
 Bonus: ${poin} XP
 `.trim()
-    conn.tebakkata = [
-        await conn.sendButton(m.chat, caption, wm, 'Bantuan', '.teka', m),
+    conn.tebakkata[id] = [
+        await conn.reply(m.chat, caption, m),
         json, poin,
-        setTimeout(async () => {
-            if (conn.tebakkata[id]) await conn.sendButton(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, wm, 'Tebak Kata', '.tebakkata', conn.tebakkata[id][0])
+        setTimeout(() => {
+            if (conn.tebakkata[id]) conn.reply(m.chat, `Waktu habis!\nJawabannya adalah *${json.jawaban}*`, conn.tebakkata[id][0])
             delete conn.tebakkata[id]
         }, timeout)
     ]
